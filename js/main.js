e6040a109a6fef705792beb1f5c878f51bfcfb2f
Sergii <seregaonma@gmail.com>
2023-10-05 12:42:07 +0300

    find-out user city

--- js/main.js
+++ js/main.js
@@ -10,3 +10,25 @@ if(userYearOfBirth === null || userYearOfBirth.trim() === '') {
 } else {
   userAge = currentYear - +userYearOfBirth;
 }
+
+let userCity = prompt('Введіть ваше місто, в якому ви проживаєте');
+while((isNaN(userCity) === false && userCity !== null && userCity.trim() !== '') || Number.parseInt(userCity)) {
+  userCity = prompt('Введіть ваше місто, в якому ви проживаєте')
+}
+switch(true) {
+  case userCity === null || userCity.trim() === '':
+    alert('Шкода, що Ви не захотіли ввести своє місто проживання');
+    userCity = null;
+    break;
+  case userCity === 'Київ':
+    userCity = 'Ти живешь у столиці України';
+    break;
+  case userCity === 'Вашингтон':
+    userCity = 'Ти живешь у столиці США';
+    break;
+  case userCity === 'Лондон': 
+    userCity = 'Ти живешь у столиці Великої Британії';
+    break;
+  default: 
+    userCity = 'Ти живеш у місті ' + userCity; 
+} 
